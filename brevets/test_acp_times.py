bbe04a608438a8f24760140a667a3d6c40dbe1db
Kyle Nielsen <kylen@uoregon.edu>
2018-02-16 16:02:11 -0800

    added more test cases

--- brevets/test_acp_times.py
+++ brevets/test_acp_times.py
@@ -6,7 +6,7 @@ import arrow
 
 base_time = arrow.get("2018-01-01T00:00:00")
 
-def test_basic():  # test a control within a bound
+def test_basic1():  # test a control within a bound
     print(open_time(100, 200, base_time.isoformat()))
     print(base_time.shift(hours=+2, minutes=+56).isoformat())
     print(base_time.isoformat())
@@ -40,6 +40,24 @@ def test_extension():  # test for a control that is 20% furhter than the declare
     print(open_actual)
     print(close_test)
     print(close_actual)
+
+    assert open_test == open_actual
+    assert close_test == close_actual
+
+def test_basic2():  # test a control within a bound
+    open_test = open_time(350, 400, base_time.isoformat())
+    open_actual = base_time.shift(hours=+10, minutes=+34).isoformat()
+    close_test =  close_time(350, 400, base_time.isoformat())
+    close_actual = base_time.shift(hours=+23, minutes=+20).isoformat()
+
+    assert open_test == open_actual
+    assert close_test == close_actual
+
+def test_long():  # test a control with a close time greater than 24h after the start time
+    open_test = open_time(500, 600, base_time.isoformat())
+    open_actual = base_time.shift(hours=+15, minutes=+28).isoformat()
+    close_test =  close_time(500, 600, base_time.isoformat())
+    close_actual = base_time.shift(hours=+33, minutes=+20).isoformat()
 
     assert open_test == open_actual
     assert close_test == close_actual
